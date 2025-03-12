0522610a39558acb356f360695ec32133806e8cf
X-some-X <x-some-x@proton.me>
2025-03-12 20:36:38 +0100

    TODO

--- modules/memory/src/level4_recursive/mod.rs
+++ modules/memory/src/level4_recursive/mod.rs
@@ -1,1 +1,8 @@
-
+// TODO: use paging (with 4 levels), with a recursive mapping of the whole physical memory.
+// For each physical block, there is a counter (size: 1B?) which holds the number of times the
+// block is referenced by a virtual block. It allows shared memory, or multi access to a memory
+// mapped register.
+// Maybe this should be an option (a bool for each block that says if it's mappable more than one
+// times. The purpose is to know to how many pages is a frame mapped, without using too much
+// memory, too much CPU or too much time.
+// The method has to be well defined.
