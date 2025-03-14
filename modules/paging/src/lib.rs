5ba70241bb7627e7524f2624ebdade19b11d99dc
X-some-X <x-some-x@proton.me>
2025-03-14 12:33:08 +0100

    - ram + paging

---
+++ modules/paging/src/lib.rs
@@ -0,0 +1,6 @@
+#![no_std]
+//! This module provides some primitives to deal with memory. It will content a physical memory
+//! manager, which represents the physical memory into virtual one, and a virtual memory manager,
+//! which provides an interface to allocate memory, find available blocks, ...
+
+
