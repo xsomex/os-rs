6ed73b7990dc015f8bc5101be204b32a25c26730
X-some-X <x-some-x@proton.me>
2025-03-16 15:40:33 +0100

    ...

--- modules/virt_memory/src/lib.rs
+++ modules/virt_memory/src/lib.rs
@@ -7,21 +7,24 @@ mod paging;
 #[cfg(feature = "paging")]
 pub use paging::*;
 
+#[derive(Debug, Clone, Copy)]
 pub struct VirtAddr(u64);
 
 pub enum VirtMemOpt {
-    Present, 
+    Present,
     Writable,
     Executable,
     UserAccessible,
 }
 
 /// TODO: add some impls (unwrap, ...)
+#[derive(Debug, Clone, Copy)]
 pub enum Result<T> {
     Ok(Error),
     Err(T),
 }
 
+#[derive(Debug, Clone, Copy)]
 pub enum Error {
     CantExtendSystem,
     /// The option can not be changed
@@ -31,7 +34,10 @@ pub enum Error {
     OptionDoesNotExist(Option<bool>),
 }
 
-pub trait VirtMem where Self: modules_common::InitModule {
+pub trait VirtMem
+where
+    Self: modules_common::InitModule,
+{
     /// find a continuous space of size 4kB blocks
     fn find_free(&mut self, size: usize) -> Result<VirtAddr>;
     /// map a virtual address to physical one. Note that it should not set/unset options, even
@@ -40,7 +46,12 @@ pub trait VirtMem where Self: modules_common::InitModule {
     where
         T: Into<u64>;
     /// Set/unset an option of the given block (is it present, readable, ...)
-    fn write_option(&mut self, virt_addr: VirtAddr, option: VirtMemOpt, value: bool) -> Result<*mut u8>;
+    fn write_option(
+        &mut self,
+        virt_addr: VirtAddr,
+        option: VirtMemOpt,
+        value: bool,
+    ) -> Result<*mut u8>;
     fn read_option(&mut self, virt_addr: VirtAddr, option: VirtMemOpt) -> Result<Option<bool>>;
     /// The space in the RAM allowed to extend the system (paging, for example)
     fn allow_extend_on<T>(&mut self, base_phys_address: T, size: usize)
--- modules/virt_memory/src/paging/mod.rs
+++ modules/virt_memory/src/paging/mod.rs
@@ -1,4 +1,19 @@
+#[repr(align(8))]
+#[derive(Debug, Clone, Copy)]
+pub struct TableEntry(u64);
+
+#[derive(Debug, Clone, Copy)]
+pub struct PageTable([TableEntry; 512]);
+
 #[derive(Debug, Clone, Copy)]
 pub struct VirtMemManager {
+    level_4: *mut PageTable,
+    extend_beginning: u64,
+    extend_size: usize,
+}
 
+impl modules_common::InitModule for VirtMemManager {
+    fn init(boot_info: &mut bootloader_api::info::BootInfo) {
+        
+    }
 }
